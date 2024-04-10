cf9ab1ef27b71f40ff4fe572277a49509f808b57
Sara Elsayeda <swe@saraelsa.net>
2024-04-09 20:54:33 -0400

    Add authoritative nameserver resolution service implementation

---
+++ src/NSDominion.Mvp.Backend/Services/Implementations/AuthoritativeNameserversResolutionService.cs
@@ -0,0 +1,43 @@
+using System.Collections.Immutable;
+using System.Net;
+using DnsClient;
+using DnsClient.Protocol;
+
+namespace NSDominion.Mvp.Backend.Services.Implementations;
+
+/// <inheritdoc />
+public sealed class AuthoritativeNameserversResolutionService(ILookupClient lookupClient) : IAuthoritativeNameserversResolutionService
+{
+    private readonly ILookupClient _lookupClient = lookupClient;
+    
+    public async Task<IReadOnlyCollection<IPAddress>> ResolveAuthoritativeNameserversAsync(
+        string domain,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentException.ThrowIfNullOrEmpty(domain);
+        
+        IDnsQueryResponse nameServerQueryResponse =
+            await _lookupClient.QueryAsync(domain, QueryType.NS, QueryClass.IN, cancellationToken);
+        
+        ImmutableList<NsRecord> nsRecords = nameServerQueryResponse.Answers
+            .NsRecords()
+            .ToImmutableList();
+        
+        if (nsRecords.IsEmpty)
+        {
+            string? parentDomain = domain.Split('.', 1).ElementAtOrDefault(1);
+            
+            return parentDomain is not null
+                ? await ResolveAuthoritativeNameserversAsync(parentDomain, cancellationToken)
+                : _lookupClient.NameServers.Select(nameServer => IPAddress.Parse(nameServer.Address)).ToImmutableList();
+        }
+        
+        IPHostEntry[] nameServerHostEntries =
+            await Task.WhenAll(nsRecords.Select(nsRecord => _lookupClient.GetHostEntryAsync(nsRecord.NSDName)));
+        
+        return nameServerHostEntries
+            .SelectMany(nameServerHostEntry => nameServerHostEntry.AddressList)
+            .ToImmutableList();
+    }
+}
